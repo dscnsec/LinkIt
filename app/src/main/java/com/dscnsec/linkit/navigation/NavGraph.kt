585e639deaeedadff25ff0547fbf9d1d8b4999cb
Somnath Paul <69642436+sp-xd@users.noreply.github.com>
2022-10-11 07:14:10 +0530

    feat: add welcome and home route
    Signed-off-by: SP-XD <69642436+SP-XD@users.noreply.github.com>

---
+++ app/src/main/java/com/dscnsec/linkit/navigation/NavGraph.kt
@@ -0,0 +1,29 @@
+package com.dscnsec.linkit.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.dscnsec.linkit.screens.HomeScreen
+import com.dscnsec.linkit.screens.WelcomeScreen
+import com.google.accompanist.pager.ExperimentalPagerApi
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Welcome.route
+    ) {
+        composable(route = Screen.Welcome.route) {
+            WelcomeScreen(navController = navController)
+        }
+        composable(route = Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
