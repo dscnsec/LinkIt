c40637328ac2fc40a6079dbe0fa35af4cace7eaa
SP-XD <69642436+SP-XD@users.noreply.github.com>
2022-10-14 03:00:39 +0530

    feat: move screen to utils & rename home to dashboard in bottomNavItem
    Signed-off-by: SP-XD <69642436+SP-XD@users.noreply.github.com>

--- app/src/main/java/com/dscnsec/linkit/utils/nav_bar_item.kt
+++ app/src/main/java/com/dscnsec/linkit/utils/nav_bar_item.kt
@@ -1,15 +1,10 @@
 package com.dscnsec.linkit.utils
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.dscnsec.linkit.R
 
 sealed class BottomNavItem( var icon : Int, var screen_route:String) {
 
-    object Home : BottomNavItem( R.raw.home, "home")
+    object Dashboard : BottomNavItem( R.raw.home, "dashboard")
     object QrScan : BottomNavItem(R.raw.qr, "qr_scan")
     object Profile : BottomNavItem(R.raw.profile, "profile")
 }
